69560756dfbb34b3642ded7e8242395f7c391f44
Patrik Lengacher <patrik.lengacher@gmail.com>
2018-10-24 22:26:11 +0200

    submisson 4 same as 3 but in pandas

---
+++ src/titanic_4_submission.py
@@ -0,0 +1,59 @@
+
+import csv as csv
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+import pandas as pd
+import titanic_helpers as th
+
+# PART 1: PREPARING THE TRAINING DATA
+print("prepare train data...")
+
+
+# Open up the CSV file into a Python object
+trainFile = "data/titanic3_train.csv"
+orig_train_data = pd.read_csv(trainFile, delimiter = ";")
+train_data = orig_train_data.copy()
+train_data = train_data[["survived","pclass","sibsp","parch","sex","age","fare","embarked"]]
+train_data = th.prepare_data(train_data)
+
+
+
+
+
+
+
+# PART 2: PREPARING THE TEST DATA
+
+# Now we have to do the same for the test data as we did for the training data
+print("prepare test data...")
+
+testFile = "data/titanic3_test.csv"
+orig_test_data = pd.read_csv(testFile, delimiter = ";")
+test_data = orig_test_data.copy()
+test_data = test_data[["pclass","sibsp","parch","sex","age","fare","embarked"]]
+test_data = th.prepare_data(test_data)
+
+# After the training and test data is created, collect the test data's ids
+test_ids = orig_test_data["id"]
+
+
+# PART 3: TRAINING AND PREDICTION
+
+# The data is now ready to go. So lets fit to the train, then predict to the test!
+print('Training...')
+forest = RandomForestClassifier(n_estimators=100)
+# Build a forest of trees from the training set (X, y)
+forest = forest.fit(train_data[["pclass","sibsp","parch","sex","age","fare","embarked"]], train_data["survived"])
+
+print('Predicting...')
+output = forest.predict(test_data[["pclass","sibsp","parch","sex","age","fare","embarked"]]).astype(np.float)
+
+# Create DataFrame for outputfile
+df = pd.DataFrame(columns = ["id","survived"])
+df["id"] = test_ids
+df["survived"] = output.astype(int)
+
+# Write the data into a file
+th.write_csv(df=df)
+
+print('Done.')
--- src/titanic_helpers.py
+++ src/titanic_helpers.py
@@ -1,6 +1,7 @@
 
 
 import pandas as pd
+import numpy as np
 import datetime
 
 
@@ -13,4 +14,61 @@ def write_csv(df):
     assert isinstance(df, pd.DataFrame)
     filename = "titanic_"+datetime.datetime.now().isoformat().replace(".", "_").replace(":","_") + ".csv"
     df[["id", "survived"]].to_csv(filename, sep=";", header=["key", "value"], index = False)
+
+
+def prepare_data(train_data):
+    #
+    # SEX: Prepare data 'sex' and store it in train_data
+    # First: get the most frequent gender
+
+    num_female = len(train_data[train_data.sex == 'female'])
+    num_male = len(train_data[train_data.sex == 'male'])
+    # Set the most frequent gender (female = 0, male = 1)
+    most_freq_gender = 0 if num_female >= num_male else 1
+    # Second: store gender data in train_data
+
+    def sex_transformation(row):
+        if row["sex"] == '':
+            return most_freq_gender  # Most freq. gender is used if 'sex' is undefined
+        if row["sex"] == 'female':
+            return 0
+        if row["sex"] == 'male':
+            return 1
+
+    train_data["sex"] = train_data.apply(lambda row: sex_transformation(row), axis = 1)
+
+    # AGE: Prepare data 'age' and store it in train_data
+    # First: get the median age
+    # Convert 'age' to float, empty values to 0
+
+    train_data["age"] = train_data.age.apply(lambda age: 0 if age == '' else float(age))
+
+    median_age = train_data["age"].median(skipna = True )
+
+    #store median age if age is undefined
+    train_data["age"] = train_data.age.apply(lambda age: 0 if age == 0 else median_age)
+
+
+    # FARE: Prepare data 'fare' and store it in train_data
+    # First: get the 'fare' and 'pclass' data
+    # Convert 'fare' to float, empty values to 0
+    train_data["fare"] = train_data.fare.apply(lambda fare: 0 if fare == '' else float(fare))
+    median_fare = train_data.groupby(["pclass"])["fare"].median(skipna=True)
+    train_data["fare"] = train_data.apply(lambda row: 0 if row["fare"] == 0 else median_fare[row["pclass"]],axis = 1)
+
+
+
+    # EMBARKED: Prepare data 'embarked' and store it in train_data
+    # First: get the most common 'embarked' value
+    embarked_data = train_data.groupby("embarked").size().reset_index(name='N')
+    mc_embarked = embarked_data.embarked[embarked_data.N == max(embarked_data.N)]
+
+    # Second: replace empty entries with the most common 'embarked' value
+    train_data["embarked"] = train_data.embarked.apply(lambda embarked: mc_embarked if embarked == '' else embarked)
+
+    # Third: convert all 'embarked' values to int
+
+    train_data["embarked"] = pd.Categorical(train_data.embarked)
+    train_data["embarked"] = train_data.embarked.cat.codes
+    return train_data
 
