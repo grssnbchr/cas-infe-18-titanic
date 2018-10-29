7a3346de42d0b58251191e59aeb044f299eeb3a1
rmonstein <monstein@gmx.net>
2018-10-29 10:13:36 +0100

    moved logistic regression into the submission file

--- src/titanic_4_submission.py
+++ src/titanic_4_submission.py
@@ -1,12 +1,11 @@
 
-import numpy as np
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 import pandas as pd
 import titanic_helpers as th
 
 # PART 1: PREPARING THE TRAINING DATA
 print("prepare train data...")
-
 
 # Open up the CSV file into a Python object
 trainFile = "../data/titanic3_train.csv"
@@ -15,8 +14,6 @@ train_data = orig_train_data.copy()
 train_data['dataset'] = 'train'
 
 # PART 2: PREPARING THE TEST DATA
-
-# Now we have to do the same for the test data as we did for the training data
 print("prepare test data...")
 
 testFile = "../data/titanic3_test.csv"
@@ -28,33 +25,51 @@ test_data['survived'] = None
 df_data = test_data.append(train_data, ignore_index=False, sort=False)
 df_data = th.prepare_data(df_data)
 
-# After the training and test data is created, collect the test data's ids
-
 # PART 3: TRAINING AND PREDICTION
 
 # The data is now ready to go. So lets fit to the train, then predict to
 # the test!
 print('Training...')
+
+predictors = ["pclass", "sibsp", "parch", "sex", "age", "fare", "embarked"]
+
+# fit a random forest
 forest = RandomForestClassifier(n_estimators=100)
-# Build a forest of trees from the training set (X, y)
-predictors = ["pclass", "sibsp", "parch", "sex", "age", "fare", "embarked", "boat"]
 forest = (forest
           .fit(df_data[df_data['dataset'] == 'train'][predictors],
                df_data[df_data['dataset'] == 'train']["survived"].astype(int))
           )
 
+# fit a logistic regression
+logreg = LogisticRegression()
+logreg.fit(df_data[df_data['dataset'] == 'train'][predictors],
+           df_data[df_data['dataset'] == 'train']["survived"].astype(int))
+
 print('Predicting...')
-output = forest.predict(df_data[df_data['dataset'] == 'test'][predictors])
+rf_out = forest.predict(df_data[df_data['dataset'] == 'test'][predictors])
+lr_out = logreg.predict(df_data[df_data['dataset'] == 'test'][predictors])
 
 # Create DataFrame for outputfile
-df = pd.DataFrame(columns=["id", "survived"])
-df["id"] = df_data[df_data['dataset'] == 'test']['id']
-df["survived"] = output
+df_rf = pd.DataFrame(columns=["id", "survived"])
+df_rf["id"] = df_data[df_data['dataset'] == 'test']['id']
+df_rf["survived"] = rf_out
 
 # Write the data into a file
-th.write_csv(df=df)
+th.write_csv(df=df_rf)
 
 # automagically submit to server
-th.submit_answer(df, custom_name='random_forest_2')
+th.submit_answer(df_rf, custom_name='random_forest_2')
+
+
+# Create DataFrame for outputfile
+df_lr = pd.DataFrame(columns=["id", "survived"])
+df_lr["id"] = df_data[df_data['dataset'] == 'test']['id']
+df_lr["survived"] = lr_out
+
+# Write the data into a file
+th.write_csv(df=df_lr)
+
+# automagically submit to server
+th.submit_answer(df_lr, custom_name='logistic_regression_2')
 
 print('Done.')
